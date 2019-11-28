2f1efb958ca4078ee18877e9dead1d0d774aaac8
Wesley Souza <wesleyadriann@gmail.com>
2019-11-27 23:24:28 -0300

    Adds inputs
    Signed-off-by: Wesley Souza <wesleyadriann@gmail.com>

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -3,10 +3,30 @@ import React, { useState } from 'react';
 import { Container, Row } from './style';
 
 function App() {
+  const [song, setSong] = useState({});
+
+  const handleChange = (e) => {
+    setSong({
+      ...song,
+      [e.target.id]: e.target.value
+    })
+  }
+
   return (
     <Container>
       <Row>
         <h1>Lyrics</h1>
+        <h3>Insert the Artist and song name, and press enter to find lyric</h3>
+        <form>
+          <label htmlFor="artist">
+            Artist
+            <input id="artist" type="text" onChange={handleChange}/>
+          </label>
+          <label htmlFor="song">
+            Song
+            <input id="song" type="text" onChange={handleChange}/>
+          </label>
+        </form>
       </Row>
     </Container>
   );
--- src/views/App/style.js
+++ src/views/App/style.js
@@ -3,16 +3,44 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     font-family: 'Sulphur Point';
 `;
 
 export const Row = styled.div`
     width: 75%;
     max-width: 1140px;
+    text-align: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
 
-    h1 {
-        text-align: center;
+    h1 {  
         font-size: 45px;
     }
+
+    form {
+        display: flex;
+        text-align: left;
+
+        label {
+            display: flex;
+            flex-direction: column;
+            margin: 5px;
+            font-size: 18px;
+
+            input {
+                padding: 10px;
+                font-size: 15px;
+                border-radius: 7px;
+                border: 1px solid #ccc;
+
+                &:focus {
+                    outline: none;
+                }
+            }
+        } 
+    }
+
 `;
