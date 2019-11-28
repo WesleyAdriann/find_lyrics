3c618d9d99ec9d77c2e3e6ba93a3b014217ea7a4
Wesley Souza <wesleyadriann@gmail.com>
2019-11-27 23:08:37 -0300

    Changes paste struct
    Signed-off-by: Wesley Souza <wesleyadriann@gmail.com>

---
+++ src/views/App/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import { Container, Row } from './style';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <h1>Lyrics</h1>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/views/App/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    font-family: 'Sulphur Point';
+`;
+
+export const Row = styled.div`
+    width: 75%;
+    max-width: 1140px;
+
+    h1 {
+        text-align: center;
+        font-size: 45px;
+    }
+`;
